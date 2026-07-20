d7049dc2a3be58a064ca9cd4481e2085c3cd9231
Denis Gnusov <denis.gnusov@spryker.com>
2026-07-20 10:58:04 +0300

    CC-39115: Fixed functional tests, updated sync functionality to add replicas if needed

--- src/SprykerEco/Zed/Algolia/Business/AlgoliaBusinessFactory.php
+++ src/SprykerEco/Zed/Algolia/Business/AlgoliaBusinessFactory.php
@@ -86,6 +86,8 @@ class AlgoliaBusinessFactory extends AbstractBusinessFactory
             $this->createProductDataFilterApplier(),
             $this->createAlgoliaConfigResolver(),
             $this->getProductFacade(),
+            $this->createSearchClientCreator(),
+            $this->createIndexConfigurator(),
         );
     }
 
